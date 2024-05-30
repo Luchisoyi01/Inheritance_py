3416d875ff9a0aa4f6463d614daef85e1eae455a
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-05-30 18:32:51 +0300

    Able to get a discount of 20%

---
+++ discount.py
@@ -0,0 +1,5 @@
+price = float(input("Enter the buying price: "))
+discount = price * 0.20
+total = price - discount
+
+print(f"The buying price is {price} and the total discount is {discount}. Therefore the buying price is: {total}")
