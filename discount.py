38bee73c810ee2e59aa36ec0e1b913d491c84756
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-05-31 22:16:28 +0300

    introduced an if statement to be used to calculate discount

--- discount.py
+++ discount.py
@@ -1,5 +1,12 @@
 price = int(input("Enter the buying price: "))
-discount = price * 0.20
+if price < 100:
+  discount = price * 0
+elif price <= 1000:
+  discount = price * 0.1
+else:
+  discount = price * 0.20
+
+#discount = price * 0.20
 total = price - discount
 
 print(f"The buying price is {price} and the total discount is {discount}. Therefore the buying price is: {total}")
