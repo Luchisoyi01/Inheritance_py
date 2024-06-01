03f038edcb32e56c9da75e1eeaca7ceeec475389
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-01 22:41:14 +0300

    Added a marker and removed the X points on the graph

--- manypoints.py
+++ manypoints.py
@@ -4,7 +4,8 @@ import numpy as np
 xpoints = np.array([1, 3, 5, 7])
 ypoints = np.array([2, 10, 2, 9])
 
-plt.plot(xpoints, ypoints)
+#plt.plot(xpoints, ypoints)
+plt.plot(ypoints, marker = '*')
 plt.show()
 
 
