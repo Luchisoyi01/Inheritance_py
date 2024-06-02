89969de9709b2d79402d647bffe618d56330cb78
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-02 23:31:44 +0300

    new changes

--- manypoints.py
+++ manypoints.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([1, 3, 5, 7])
+#xpoints = np.array([1, 3, 5, 7])
 ypoints = np.array([2, 10, 2, 9])
 
 #plt.plot(xpoints, ypoints)
