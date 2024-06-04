97af58f20b91d09ae5aeccbe3b728ae5e94b8105
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-04 14:52:25 +0300

    A bar graph  of points scored by different teams

---
+++ pointsgraph.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as points
+
+x = np.array(["Panthers", "Ravens", "Shooting Stars", "Rockets"])
+y =np.array([6,4,9,8])
+
+points.title("Points scored by teams")
+points.xlabel("Team name")
+points.ylabel("Number of points")
+
+points.bar(x,y, color="red")
+points.grid(axis="y")
+points.show()
