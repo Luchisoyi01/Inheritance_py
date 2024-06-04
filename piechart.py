c17885acdf47ede229ea96ee147b63bab414e64a
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-04 15:56:02 +0300

    Added the title and the legend in the pie chart

--- piechart.py
+++ piechart.py
@@ -4,7 +4,13 @@ import matplotlib.pyplot as pie
 y= np.array([203, 103, 101, 73, 60])
 mylabels= ["Apple", "Banana", "Grapefruit", "Raspberry", "orange"]
 
+font1 = {
+  "family":"times new roman",
+  "fontweight":"bold",
+  "size": 20
+}
 
+pie.title("Fruit distribution in Meru", fontdict = font1 , loc="left")
 pie.pie(y, labels = mylabels)
-pie.legend()
+pie.legend(title = "Fruits in Meru:")
 pie.show()
