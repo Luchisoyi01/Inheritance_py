05ee78f555d42f9241a2ca52737010add290b052
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-08 00:20:03 +0300

    the graph show a randon normal distribution

--- data.py
+++ data.py
@@ -1,7 +1,13 @@
 import numpy
 import matplotlib.pyplot as plt
 
-x = numpy.random.uniform(0.0, 10.0, 100)
-
-plt.hist(x, 5)
+x = numpy.random.normal(10.0, 1.0, 1000000)
+#y = numpy.random.uniform(10.0, 1.0, 1000000)
+plt.hist(x, 100)
+#plt.hist(y, 100)
 plt.show()
+
+'''
+A normal distribution graph is also known as the bell curve 
+because of it's characteristic shape of a bell.
+'''
