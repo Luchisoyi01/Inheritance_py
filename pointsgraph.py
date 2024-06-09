41eda65d248758ff8d7bc985b41894c2d3d82222
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-09 19:08:12 +0300

    changed the graph's apperance

--- pointsgraph.py
+++ pointsgraph.py
@@ -8,6 +8,6 @@ points.title("Points scored by teams")
 points.xlabel("Team name")
 points.ylabel("Number of points")
 
-points.bar(x,y, color="red")
-points.grid(axis="y")
+points.bar(x,y, color="yellow")
+#points.grid(axis="y")
 points.show()
