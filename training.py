594c2f1ab43f987327f74b863365920a8004fdc8
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-12 22:01:25 +0300

    training model complete

--- training.py
+++ training.py
@@ -5,5 +5,8 @@ numpy.random.seed(2)
 x = numpy.random.normal(3, 1, 100)
 y = numpy.random.normal(150, 40, 100) / x
 
-plt.scatter(x, y)
+train_x = x[:80]
+train_y = y[:80]
+
+plt.scatter(train_x, train_y)
 plt.show()
